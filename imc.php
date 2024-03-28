0d2d42b48bb8c4748ab8b1fdd0c8416190bd8ee0
sasadevs <131981869+sasadevs@users.noreply.github.com>
2024-03-27 22:17:09 -0300

    Add files via upload

--- imc.php
+++ imc.php
@@ -15,7 +15,30 @@
 
 		$resp = imc ($peso, $altura);
 
-		printf ("Olá! $nome seu peso é $peso kg , sua altura é $altura m e seu IMC é: %.2f " , $resp);
+
+		if ($resp <= 18.5){
+
+			echo "$nome você está abaixo do peso! <br />";
+		} elseif ($resp >= 18.6 && $resp <= 24.9) {
+
+			echo "$nome você está no peso ideal! <br />";
+
+		} elseif ($resp >= 25.0 && $resp <= 29.9) {
+			
+			echo "$nome você está levemente acima do peso! <br />";
+		} elseif ($resp >= 30.0 && $resp <= 34.9) {
+			
+			echo "$nome você está em obesidade grau I! <br />";
+		} elseif ($resp >= 35.0 && $resp <= 39.9) {
+			
+			echo "$nome você está em obesidade grau II! (severa) <br />";
+		} elseif ($resp >= 40.0){
+
+			echo "$nome você está em obesidade III! (mórbida) <br />";
+		}
+
+
+		printf ("Seu peso é $peso kg, sua altura é $altura m e seu IMC é: %.2f " , $resp);
 
 
 
