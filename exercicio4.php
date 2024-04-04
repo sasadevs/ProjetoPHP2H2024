16e70d6979b90170fe140310b0b977f094602dc7
sasadevs <sarinha.chate@gmail.com>
2024-04-03 22:15:53 -0300

    tentativa ex 5 (fracassada)

---
+++ exercicio4.php
@@ -0,0 +1,15 @@
+<?php 
+	
+	$cont = 0;
+
+	$num1 = 5;
+	$num2 = 4;
+	$num3 = 2;
+
+	for($cont = 0; $cont <= 3; $cont--){
+
+		
+
+	}
+
+ ?>
---
+++ exercicio5letraA.php
@@ -0,0 +1,49 @@
+<?php 
+
+	// DUPLA: Sara Chaves e Daniel Alves
+	
+
+	$altura1 = 1.65;
+	$altura2 = 1.76;
+	$altura3 = 1.86;
+	$altura4 = 1.90;
+	$altura5 = 1.57;
+	$altura6 = 1.60;
+	$altura7 = 1.55;
+	$altura8 = 1.49;
+	$altura9 = 1.70;
+	$altura10 = 1.75;
+
+
+	$idade1 = 18;
+	$idade2 = 26;
+	$idade3 = 18;
+	$idade4 = 26;
+	$idade5 = 18;
+	$idade6 = 26;
+	$idade7 = 18;
+	$idade8 = 26;
+	$idade9 = 18;
+	$idade10 = 26;
+
+	$sexo1 = "Feminino";
+	$sexo2 = "Masculino";
+	$sexo3 = "Masculino";
+	$sexo4 = "Feminino";
+	$sexo5 = "Masculino";
+	$sexo6 = "Masculino";
+	$sexo7 = "Feminino";
+	$sexo8 = "Masculino";
+	$sexo9 = "Feminino";
+	$sexo10 = "Masculino";
+
+	echo $idade max()
+
+
+
+
+	
+	
+
+
+ ?>
