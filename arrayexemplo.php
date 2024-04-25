1702c9a2cb463c51583006515085fa13047346c3
sasadevs <sarinha.chate@gmail.com>
2024-04-24 22:16:21 -0300

    iniciando com array

---
+++ arrayexemplo.php
@@ -0,0 +1,27 @@
+<?php 
+
+	$nome = array("Maria", "João", "Joaquim", "Josefina");
+
+
+	echo "Nomes no array: </br>";
+
+	foreach ($nome as $nomes) {
+		
+		echo "<br/>".$nomes;
+
+	}
+
+
+	$num = array(1, 2, 3, 4, 5);
+
+
+	echo "</br></br>Números no array: </br></br>";
+
+	foreach ($num as $valor) {
+		
+		echo "Valor é $valor <br/>";
+	}
+
+
+
+ ?>
